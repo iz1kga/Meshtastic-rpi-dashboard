4ea26571c6541c446a39c702bfc17cc91fad7dd5
iz1kga <iz1kga@gmail.com>
2021-02-17 16:16:11 +0100

    working nodes Table

--- server.py
+++ server.py
@@ -27,12 +27,12 @@ class Root(object):
     @cherrypy.expose
     def index(self):
         return """<html>
-    <head>
-      <script type='application/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'></script>
-      <script type='application/javascript'>
+        <head>
+        <script type='application/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'></script>
+        <script type='application/javascript'>
         var jsonData;
+        var nodeList = [];
         $(document).ready(function() {
-
           websocket = '%(scheme)s://%(host)s:%(port)s/ws';
           if (window.WebSocket) {
             ws = new WebSocket(websocket);
@@ -56,11 +56,67 @@ class Root(object):
           ws.onmessage = function (evt) {
              $('#chat').val($('#chat').val() + evt.data + '\\n');
              jsonData = JSON.parse(evt.data)
-             $('#nodes').val("");
+
+
+             nodeList = [];
              for (let [key, value] of Object.entries(jsonData.nodes)) {
-                 console.log(key, value);
-                 $('#nodes').val($('#nodes').val() + key +'\\n');
+                 nodeList.push({"id":key,
+                                "user":value.user.longName,
+                                "pos":{"lat":parseFloat(value.position.latitude).toFixed(4),
+                                       "lon":parseFloat(value.position.longitude).toFixed(4),
+                                       "alt":parseInt(value.position.altitude)}, 
+                                "batt":parseInt(value.position.batteryLevel),
+                                "lh":value.position.time});
              }
+
+
+             nodeList.sort((a,b) => {
+                                    // two undefined values should be treated as equal ( 0 )
+                                    if( typeof a.lh === 'undefined' && typeof b.lh === 'undefined' )
+                                        return 0;
+                                    // if a is undefined and b isn't a should have a lower index in the array
+                                    else if( typeof a.lh === 'undefined' )
+                                        return 1;
+                                    // if b is undefined and a isn't a should have a higher index in the array
+                                    else if( typeof b.lh === 'undefined' )
+                                        return -1;
+                                    // if both lhs are defined compare as normal
+                                    else
+                                        return b.lh - a.lh;
+                                    });
+
+             //Nodes Table
+             $('#nodes').val("");
+             $("#nodesTable tbody").empty();
+             nodeList.forEach(element => {
+                 var elePos = "";
+                 var eleBatt = "";
+                 var eleLh = "";
+                 if(!isNaN(element.lh))
+                     eleLh = new Date(element.lh*1000).toLocaleString();
+                 else
+                     eleLh = "---";
+
+                 if(isNaN(element.pos.lat))
+                     elePos = "---";
+                 else
+                     elePos = element.pos.lat+"°, "+element.pos.lon+"°, "+element.pos.alt+" m" ;
+
+                 if(isNaN(element.batt))
+                     eleBatt = "---";
+                 else
+                     eleBatt = element.batt+"&#37;";
+
+                 $("#nodesTable tbody").append("<tr><td>"+element.id+
+                                               "</td><td>"+element.user+
+                                               "</td><td>"+elePos+
+                                               "</td><td>"+eleBatt+
+                                               "</td><td>"+
+                                               "</td><td>"+eleLh+
+                                               "</td></tr>")});
+
+             //"</td><td>"+parseFloat(value.position.longitude).toFixed(4)+", "+parseFloat(value.position.longitude).toFixed(4)+", "+value.position.altitude+
+
              if(jsonData.packet.decoded.data.portnum == "TEXT_MESSAGE_APP")
              {
                  $('#messages').val($('#messages').val() + jsonData.packet.fromId +": "+ jsonData.packet.decoded.data.text  + '\\n');
@@ -90,7 +146,20 @@ class Root(object):
       </form>
       <textarea id='nodes' cols='50' rows='10'></textarea>
       <textarea id='messages' cols='50' rows='10'></textarea>
-
+      <table id="nodesTable" class="table table-bordered table-condensed table-striped">
+          <thead>
+              <tr>
+                  <th>ID</th>
+                  <th>User</th>
+                  <th>Position</th>
+                  <th>Battery</th>
+                  <th>SNR</th>
+                  <th>Last Heard</th>
+              </tr>
+          </thead>
+          <tbody>
+          </tbody>
+      </table>
     </body>
     </html>
     """ % {'username': "User%d" % random.randint(0, 100), 'host': self.host, 'port': self.port, 'scheme': self.scheme}
