a248032557f8817c919001d643c3502a20b1f8cd
iz1kga <iz1kga@gmail.com>
2021-03-05 15:12:10 +0100

    release 0.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "iMesh-Dashboard",
-    version = "0.0.7",
+    version = "0.0.8",
     author = "iz1kga",
     author_email = "iz1kga@gmail.com",
     description = "Create Dashboard for serial connected meshtastic device",
