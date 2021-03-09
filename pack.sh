4640311729ecb744ca2c854c8e283ee32aa44144
iz1kga <iz1kga@gmail.com>
2021-03-09 12:40:52 +0100

    template arrange

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
 sudo pip3 uninstall -y iMesh-Dashboard
 sudo rm -r dist/
+sudo rm -r /usr/local/iMeshDashboard/
 python3 setup.py sdist bdist_wheel --universal
 sudo pip3 install ./dist/iMesh-Dashboard-*.tar.gz
