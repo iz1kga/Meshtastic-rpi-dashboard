eb585ed29b4ac4b13abaf7617dce8edde2f4ac51
iz1kga <iz1kga@gmail.com>
2021-03-22 14:57:17 +0100

    Release 0.1.12 - FIX GPIO ok

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "iMesh-Dashboard",
-    version = "0.1.11",
+    version = "0.1.12",
     author = "iz1kga",
     author_email = "iz1kga@gmail.com",
     description = "Create Dashboard for serial connected meshtastic device",
